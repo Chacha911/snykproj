3bb03d5cf66e972ecd4a693b6a2e19be0011c689
srmono <bvsrao91@gmail.com>
2021-07-26 20:06:23 +0530

    final version update

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const connectDB = require("./config/db");
+const path = require('path');
+
 const app = express();
 
 
@@ -14,15 +16,21 @@ app.use(cors());
 app.use(express.urlencoded()); // Parse URL-encoded bodies
 app.use(express.json());// User to parese JSON bodies
 
-app.get("/", (req, res) => res.send("Eteam App is running") );
-
 // Define Routes
-
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 
+//Serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
